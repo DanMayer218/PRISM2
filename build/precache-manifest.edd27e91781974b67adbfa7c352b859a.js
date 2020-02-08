3b19dcfc68a815f87c0e99ec3d6eed1885dafa45
Daniel Mayer <danmayercraigs@gmail.com>
2020-02-08 09:49:31 -0500

    created a build folder

---
+++ build/precache-manifest.edd27e91781974b67adbfa7c352b859a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab40a7027115c0fab7449aaa7a0fc086",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f58035c2bc65db57ac6",
+    "url": "/static/css/main.70869172.chunk.css"
+  },
+  {
+    "revision": "d51e90d4598f0b5f629a",
+    "url": "/static/js/2.808b529c.chunk.js"
+  },
+  {
+    "revision": "5f45b28297cb38ff18408778ee2445e6",
+    "url": "/static/js/2.808b529c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f58035c2bc65db57ac6",
+    "url": "/static/js/main.063fc9cb.chunk.js"
+  },
+  {
+    "revision": "f26bd930a0b00aa70b7a",
+    "url": "/static/js/runtime-main.0697f1f0.js"
+  }
+]);
